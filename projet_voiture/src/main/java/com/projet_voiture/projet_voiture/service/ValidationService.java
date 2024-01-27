ecde2b2fa7cddd7d57111dd793153f712f1e8037
Rebeka-1892 <etu1892@gmail.com>
2024-01-27 08:02:20 +0300

    validation

--- projet_voiture/src/main/java/com/projet_voiture/projet_voiture/service/ValidationService.java
+++ projet_voiture/src/main/java/com/projet_voiture/projet_voiture/service/ValidationService.java
@@ -7,19 +7,10 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.projet_voiture.projet_voiture.modele.HistoriqueValidation;
-import com.projet_voiture.projet_voiture.modele.Tresorerie;
 import com.projet_voiture.projet_voiture.modele.Validation;
-import com.projet.voiture_occasion.modele.NombreVoitureVenduParMois;
+import com.projet_voiture.projet_voiture.modele.NombreVoitureVenduParMois;
 import com.projet_voiture.projet_voiture.repository.ValidationRepository;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -29,18 +20,6 @@ import java.util.stream.Collectors;
 public class ValidationService {
     @Autowired
     private ValidationRepository repository;   
-
-    // @Autowired
-    // private AnnonceService annonceService;
-
-    // @Autowired
-    // private CommissionService commissionService;
-
-    // @Autowired
-    // private TresorerieService tresorerieService;
-
-    // @Autowired
-    // private HistoriqueValidationService historiqueValidationService;
 
     @Autowired
     private MongoTemplate mongoTemplate;
@@ -66,54 +45,6 @@ public class ValidationService {
         Validation.setIdvalidation(UUID.randomUUID().toString().split("-")[0]);
         return repository.save(Validation);
     }
-
-    // public List<Validation> findAll() {
-    //     return repository.findAll();
-    // }
-    // public Validation findById(String ValidationId){
-    //     return repository.findById(ValidationId).get();
-    // }
-
-    // @Transactional
-    // public Validation updateValidation(Validation ValidationRequest){
-    //     Validation existingValidation = repository.findById(ValidationRequest.getIdvalidation()).get();
-
-    //     HistoriqueValidation historiqueValidation = new HistoriqueValidation();
-    //     historiqueValidation.setIdvalidation(existingValidation.getIdvalidation());
-    //     historiqueValidation.setIdannonce(existingValidation.getIdannonce());
-    //     historiqueValidation.setEtat(existingValidation.getEtat());
-    //     historiqueValidation.setDatemodif(existingValidation.getDatemodif());
-
-    //     historiqueValidationService.insertHistoriqueValidation(historiqueValidation);
-
-    //     existingValidation.setEtat(ValidationRequest.getEtat());
-    //     existingValidation.setIdannonce(ValidationRequest.getIdannonce());
-    //     existingValidation.setDatemodif(LocalDateTime.now());
-        
-
-    //     if(existingValidation.getEtat() == 3) {
-    //         double pourcentage = commissionService.getLastCommission().getValeur();
-    //         double prixVoiture = annonceService.findById(existingValidation.getIdannonce()).getPrix();
-
-    //         double gainSite = prixVoiture *(pourcentage/100);
-
-    //         Tresorerie tresorerie = new Tresorerie();
-    //         tresorerie.setIdannonce(existingValidation.getIdannonce());
-    //         tresorerie.setEntre(gainSite);
-    //         tresorerie.setSortie(0);
-    //         LocalDateTime currentDate = LocalDateTime.now();
-    //         Instant instant = currentDate.toInstant(ZoneOffset.UTC);
-    //         tresorerie.setDatemouvement(Timestamp.from(instant));
-
-    //         tresorerieService.insertTresorerie(tresorerie);
-    //     }
-    //     return repository.save(existingValidation);
-    // }
-
-    // public String deleteValidation(String ValidationId){
-    //     repository.deleteById(ValidationId);
-    //     return ValidationId+" Validation deleted from dashboard ";
-    // }
 
     public List<NombreVoitureVenduParMois> getNombreVenteParMois() {
         Aggregation aggregation = Aggregation.newAggregation(
